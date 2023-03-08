c2b50a034361179552de059713469bd2605f0919
nguu0123 <nguu0123@gmail.com>
2023-03-08 21:38:46 +0200

    08 03

---
+++ ray_deployment/client/client.py
@@ -0,0 +1,117 @@
+import requests, time, argparse, csv
+from threading import Thread
+import os, random, json, cv2, threading
+import numpy as np
+from memory_profiler import profile
+from qoa4ml.connector.amqp_connector import Amqp_Connector
+from qoa4ml.collector.amqp_collector import Amqp_Collector
+from qoa4ml.utils import load_config
+
+parser = argparse.ArgumentParser(description="Data processing")
+parser.add_argument("--th", help="number concurrent thread", default=1)
+parser.add_argument("--sl", help="time sleep", default=-1.0)
+args = parser.parse_args()
+concurrent = int(args.th)
+time_sleep = float(args.sl)
+# url = 'http://192.168.1.148:5000/inference_aaltosea'
+url = "http://127.0.0.1:8000/"
+
+
+def sender(num_thread):
+    count = 0
+    start_time = time.time()
+    while time.time() - start_time < 600:
+        print("This is thread: ", num_thread, "Starting request: ", count)
+        ran_file = random.choice(os.listdir("./image"))
+        files = {"image": open("./image/" + ran_file, "rb")}
+        response = requests.post(url, files=files)
+        prediction = response.json()
+        print(prediction["prediction"]["yolo5"])
+        print(prediction["prediction"]["yolo8"])
+        image = np.asarray((prediction["image"]))
+
+        # Comment this to use multi-thread
+        cv2.imshow("lable", image.astype(np.uint8))
+        cv2.waitKey(2000)
+        time.sleep(10000)
+
+        print("Thread - ", num_thread, " Response:")
+        count += 1
+        if time_sleep == -1:
+            time.sleep(1)
+        else:
+            time.sleep(time_sleep)
+
+
+# 1 Thread
+
+class Client:
+    def __init__(self, connector_config: dict, log: bool = False):
+        # self.amqp_connector = Amqp_Connector(connector_config, log)
+        self.correct_detect = 0
+        self.request_sent = 0 
+        self.request_log_comlums   = ["Image ID", "Response time"]
+        self.inference_log_columns = ["Image ID", "Model", "Class", "Correctly detect", "Number of objects detected", 'Width', 
+                                      'Height', 'Object width', 'Object height', 'Object to Image percentage']
+    def send_message(self):
+        ran_class = random.choice(os.listdir("./images/"))
+        ran_file = random.choice(os.listdir("./images/{}".format(ran_class)))
+        file = open("./images/" + ran_class + "/" + ran_file, "rb").read()
+        start_time = time.time()
+        response = requests.post(url, files={"data": file, "file name": ran_file})
+        prediction = response.json()
+        np_array = np.frombuffer(file, np.uint8)
+        im = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        height, width, _ = im.shape
+        self.monitor(time.time() - start_time, prediction["prediction"], ran_file, ran_class, height, width)
+
+    def monitor(self, response_time, inference_result, image_id, true_class, image_height, image_width):
+        for model in inference_result.keys():
+            if inference_result[model]:
+                data = inference_result[model] 
+                object_num = 0
+                correctly_detect = 0
+                object_height = 0
+                object_width = 0
+                percentage = 0 
+                for detected_object in data:
+                    cur_object = detected_object["object_{}".format(object_num)]
+                    if isinstance(cur_object, list):
+                        cur_object = cur_object[0]
+                    if cur_object["name"] == true_class:
+                        correctly_detect = 1
+                        object_width  = cur_object['xmax'] - cur_object['xmin']
+                        object_height = cur_object['ymax'] - cur_object['ymin']
+                        object_size = object_width * object_height 
+                        percentage = object_size / (image_height * image_width)
+                    object_num += 1
+                self.append_to_log([image_id, model, true_class, correctly_detect, object_num, 
+                                    image_width, image_height, object_width, object_height, percentage], 'inference_log')
+        self.append_to_log([image_id, response_time], 'request_log')
+    def append_to_log(self, data, file_name):
+        path = "../logs/{}.csv".format(file_name)
+        if not os.path.exists(path):
+            with open(path, "w") as f:
+                writer = csv.writer(f)
+                if file_name == "request_log":
+                    writer.writerow(self.request_log_comlums)
+                elif file_name == 'inference_log':
+                    writer.writerow(self.inference_log_columns)
+        with open(path, "a") as f_object:
+            writer_object = csv.writer(f_object)
+            writer_object.writerow(data)
+
+if __name__ == '__main__':
+    connector_config = load_config("./client_connector.json")
+    client = Client(connector_config)
+    i = 0 
+    try:
+        while i < 1000:
+            client.send_message()
+            i += 1 
+    except KeyboardInterrupt:
+        print('interrupted!')
+# Multi-thrad
+# for i in range(concurrent):
+#     t = Thread(target=sender,args=[i])
+#     t.start()
---
+++ ray_deployment/server/deployment.py
@@ -0,0 +1,167 @@
+import argparse
+import json
+import os
+import os.path
+import random
+import time
+import uuid
+import csv
+from csv import writer
+from datetime import datetime
+
+import aggregation
+import cv2
+import numpy as np
+import pandas as pd
+import pymongo
+import qoa4ml.utils as qoa_utils
+import ray
+import requests
+from fastapi import FastAPI
+from qoa4ml.reports import Qoa_Client
+from qoa4ml.utils import load_config
+from ray import serve
+from ray.serve.deployment_graph import InputNode
+from ray.serve.drivers import DAGDriver
+from ray.serve.handle import RayServeDeploymentHandle
+from ray.serve.http_adapters import json_request
+from starlette.requests import Request
+from yolov5.yolov5 import Yolo5
+from yolov8.yolov8 import Yolo8
+
+
+@ray.remote
+def enhance_image(image):
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    enhanced_im = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+    return enhanced_im
+
+
+@ray.remote
+def mean_aggregate(predictions):
+    agg_prediction = aggregation.agg_mean(predictions)
+    return agg_prediction
+
+
+@ray.remote
+def max_aggregate(predictions):
+    agg_prediction = aggregation.agg_max(predictions)
+    return agg_prediction
+
+
+@serve.deployment
+class Yolo8Inference:
+    def __init__(self, param):
+        self.model = Yolo8(param)
+        self.param = param
+
+    def predict(self, image):
+        prediction, pre_img = self.model.yolov8_inference(image)
+        return {"prediction": prediction, "image": pre_img, "model_name": self.param}
+
+
+@serve.deployment
+class Yolo5Inference:
+    def __init__(self, param):
+        self.model = Yolo5(param)
+        self.param = param
+        self.inference_time = 0
+
+    def predict(self, image):
+        prediction, pre_img = self.model.yolov5_inference(image)
+        return {"prediction": prediction, "image": pre_img, "model_name": self.param}
+
+
+@serve.deployment
+class Ensemble_ML:
+    def __init__(self, yolo5, yolo8):
+        self.yolo5 = yolo5
+        self.yolo8 = yolo8
+
+    async def inference(self, data):
+        np_array = np.frombuffer(data, np.uint8)
+        im = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        en_im = enhance_image.remote(im)
+        response = {}
+        yl5 = await self.yolo5.predict.remote(en_im)
+        yl8 = await self.yolo8.predict.remote(en_im)
+        yl5 = ray.get(yl5)
+        yl8 = ray.get(yl8)
+        agg_pred = await max_aggregate.remote(yl5["prediction"] | yl8["prediction"])
+        response["prediction"] = {
+            "aggregated": agg_pred,
+            yl5["model_name"]: yl5["prediction"][yl5["model_name"]],
+            yl8["model_name"]: yl8["prediction"][yl8["model_name"]],
+        }
+        response["image"] = yl8["image"]
+        return response
+
+
+@serve.deployment
+class MostBasicIngress:
+    def __init__(self, ensemble):
+        self.ensemble = ensemble
+        self.respond_time = 0
+        self.request_served = 0
+        self.log_file_name = "server_log.csv"
+        self.models_log_comlumns = [
+            "Image ID",
+            "Model",
+            "Object detected",
+            "Confidence",
+        ]
+        self.server_log_columns = ["Image ID", "Response time"]
+        self.models_log = pd.DataFrame(columns=self.models_log_comlumns)
+        self.server_log = pd.DataFrame(columns=self.server_log_columns)
+
+    async def __call__(self, request: Request):
+        start_time = time.time()
+        time_stamp = datetime.fromtimestamp(time.time())
+        files = await request.form()
+        data = await files["data"].read()
+        image_id = await files["file name"].read()
+        response = await self.ensemble.inference.remote(data)
+        response = ray.get(response)
+        self.monitor(start_time, time_stamp, response["prediction"], image_id)
+        return response
+
+    def monitor(self, start_time, times_stamp, inference_result, image_id):
+        for model in inference_result.keys():
+            if inference_result[model]:
+                data = inference_result[model] 
+                object_num = 0
+                for detected_object in data:
+                    cur_object = detected_object["object_{}".format(object_num)]
+                    if isinstance(cur_object, list):
+                        cur_object = cur_object[0]
+                    self.append_to_log(
+                        [
+                            image_id,
+                            model,
+                            cur_object["name"],
+                            cur_object["confidence"],
+                        ],
+                        "models_log",
+                    )
+                    object_num += 1
+        self.append_to_log([image_id, time.time() - start_time], 'server_log')
+        self.request_served += 1
+
+    def append_to_log(self, data, file_name):
+        path = "../logs/{}.csv".format(file_name)
+        if not os.path.exists(path):
+            with open(path, "w") as f:
+                writer = csv.writer(f)
+                if file_name == "server_log":
+                    writer.writerow(self.server_log_columns)
+                elif file_name == "models_log":
+                    writer.writerow(self.models_log_comlumns)
+        with open(path, "a") as f_object:
+            writer_object = csv.writer(f_object)
+            writer_object.writerow(data)
+
+models_config = load_config("../config/models.json")
+yolo5 = Yolo5Inference.bind(models_config['yolo5'])
+yolo8 = Yolo8Inference.bind(models_config['yolo8'])
+ensemble = Ensemble_ML.bind(yolo5, yolo8)
+server = MostBasicIngress.bind(ensemble)
