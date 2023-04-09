57da4d11b57ebcb181b611ec0246e9b03cb6db9b
nguu0123 <nguu0123@gmail.com>
2023-04-09 14:58:45 +0300

    add requirement

---
+++ ray_deployment/server/deploytmentSync.py
@@ -0,0 +1,204 @@
+import argparse
+import json
+from math import acos
+import os
+import os.path
+import random
+import time
+import uuid
+import csv
+from csv import writer
+from datetime import datetime
+from functools import reduce
+
+import aggregation
+import cv2
+import numpy as np
+import pandas as pd
+import pymongo
+import qoa4ml.utils as qoa_utils
+import ray
+import requests
+from fastapi import FastAPI
+from qoa4ml.reports import Qoa_Client
+from qoa4ml.utils import load_config
+from ray import serve
+from ray.serve.deployment_graph import InputNode
+from ray.serve.drivers import DAGDriver
+from ray.serve.handle import RayServeDeploymentHandle
+from ray.serve.http_adapters import json_request
+from starlette.requests import Request
+from yolov5.yolov5 import Yolo5
+from yolov8.yolov8 import Yolo8
+from prov.prov_function import captureInputData, capture, captureModel 
+
+@ray.remote
+@capture(activityType='preprocessing')
+def enhance_image(data):
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    enhanced_im = cv2.filter2D(src=data.data, ddepth=-1, kernel=kernel)
+    return enhanced_im
+
+
+@ray.remote
+@capture(activityType='ensemble')
+def mean_aggregate(predictions):
+    agg_prediction = aggregation.agg_mean(reduce(lambda a, b: a.data | b.data, predictions))
+    return agg_prediction
+
+
+@ray.remote
+@capture(activityType='ensemble')
+def max_aggregate(predictions):
+    agg_prediction = aggregation.agg_max(reduce(lambda a, b: a.data | b.data, predictions))
+    return agg_prediction
+
+
+@serve.deployment
+class Yolo8Inference:
+    @captureModel
+    def __init__(self, param):
+        self.model = Yolo8(param)
+        self.param = param
+
+    @capture(activityType='predict')
+    def predict(self, data):
+        prediction, pre_img = self.model.yolov8_inference(data.data)
+        return {"prediction": prediction, "image": pre_img, "model_name": self.param, "QoA": self.extractQoA(prediction)}
+
+    def extractQoA(self, prediction):
+        report = {}
+        for model in prediction.keys():
+            if prediction[model]:
+                data = prediction[model] 
+                object_num = 0
+                for detected_object in data:
+                    cur_object = detected_object["object_{}".format(object_num)]
+                    if isinstance(cur_object, list):
+                        cur_object = cur_object[0]
+                    report[cur_object["name"]] =  cur_object["confidence"]
+                    object_num += 1
+                report["number of object"] = object_num
+        return report
+
+@serve.deployment
+class Yolo5Inference:
+    @captureModel
+    def __init__(self, param):
+        self.model = Yolo5(param)
+        self.param = param
+        self.inference_time = 0
+
+    @capture(activityType='predict')
+    def predict(self, data):
+        prediction, pre_img = self.model.yolov5_inference(data.data)
+        return {"prediction": prediction, "image": pre_img, "model_name": self.param, "QoA": self.extractQoA(prediction)}
+
+    def extractQoA(self, prediction):
+        report = {}
+        for model in prediction.keys():
+            if prediction[model]:
+                data = prediction[model] 
+                object_num = 0
+                for detected_object in data:
+                    cur_object = detected_object["object_{}".format(object_num)]
+                    if isinstance(cur_object, list):
+                        cur_object = cur_object[0]
+                    report[cur_object["name"]] =  cur_object["confidence"]
+                    object_num += 1
+                report["number of object"] = object_num
+        return report
+@serve.deployment
+class Ensemble_ML:
+    def __init__(self, yolo5, yolo8):
+        self.yolo5 = yolo5
+        self.yolo8 = yolo8
+    async def inference(self, data):
+        np_array = np.frombuffer(data, np.uint8)
+        im = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        im =  captureInputData(im)
+        self.assessDataQuality(data=im)
+        en_im = enhance_image.remote(data=im)
+        response = {}
+        yl5 = await self.yolo5.predict.remote(data=en_im)
+        yl8 = await self.yolo8.predict.remote(data=en_im)
+        yl5 = ray.get(yl5)
+        yl8 = ray.get(yl8)
+        agg_pred = await max_aggregate.remote(predictions = [yl5.getAs("prediction"), yl8.getAs("prediction")] )
+        response["prediction"] = {
+            "aggregated": agg_pred.data, yl5.data["model_name"]: yl5.data["prediction"][yl5.data["model_name"]],
+            yl8.data["model_name"]: yl8.data["prediction"][yl8.data["model_name"]],
+        }
+        response["image"] = yl8.data["image"]
+        return response
+    @capture(activityType='assessDataQuality')
+    def assessDataQuality(self, data):
+        height, width, _ = data.data.shape
+        return {"height": height, "width": width}
+
+@serve.deployment
+class MostBasicIngress:
+    def __init__(self, ensemble):
+        self.ensemble = ensemble
+        self.respond_time = 0
+        self.request_served = 0
+        self.log_file_name = "server_log.csv"
+        self.models_log_comlumns = [
+            "Image ID",
+            "Model",
+            "Object detected",
+            "Confidence",
+        ]
+        self.server_log_columns = ["Image ID", "Response time"]
+        self.models_log = pd.DataFrame(columns=self.models_log_comlumns)
+        self.server_log = pd.DataFrame(columns=self.server_log_columns)
+    async def __call__(self, request: Request):
+        start_time = time.time()
+        time_stamp = datetime.fromtimestamp(time.time())
+        files = await request.form()
+        data = await files["data"].read()
+        image_id = await files["file name"].read()
+        response = await self.ensemble.inference.remote(data)
+        response = ray.get(response)
+        return response
+
+    def monitor(self, start_time, times_stamp, inference_result, image_id):
+        for model in inference_result.keys():
+            if inference_result[model]:
+                data = inference_result[model] 
+                object_num = 0
+                for detected_object in data:
+                    cur_object = detected_object["object_{}".format(object_num)]
+                    if isinstance(cur_object, list):
+                        cur_object = cur_object[0]
+                    self.append_to_log(
+                        [
+                            image_id,
+                            model,
+                            cur_object["name"],
+                            cur_object["confidence"],
+                        ],
+                        "models_log",
+                    )
+                    object_num += 1
+        self.append_to_log([image_id, time.time() - start_time], 'server_log')
+        self.request_served += 1
+
+    def append_to_log(self, data, file_name):
+        path = "../logs/{}.csv".format(file_name)
+        if not os.path.exists(path):
+            with open(path, "w") as f:
+                writer = csv.writer(f)
+                if file_name == "server_log":
+                    writer.writerow(self.server_log_columns)
+                elif file_name == "models_log":
+                    writer.writerow(self.models_log_comlumns)
+        with open(path, "a") as f_object:
+            writer_object = csv.writer(f_object)
+            writer_object.writerow(data)
+
+models_config = load_config("../config/models.json")
+yolo5 = Yolo5Inference.bind(param=models_config['yolo5'])
+yolo8 = Yolo8Inference.bind(param=models_config['yolo8'])
+ensemble = Ensemble_ML.bind(yolo5, yolo8)
+server = MostBasicIngress.bind(ensemble)
---
+++ ray_deployment/server/prov/prov_function_sync.py
@@ -0,0 +1,327 @@
+import time
+import uuid
+import json
+import functools
+import asyncpg
+import os
+import psutil
+from datetime import datetime
+from qoa4ml.utils import load_config, get_proc_cpu, get_proc_cpu
+from torch.nn.functional import linear
+
+class Data:
+    def __init__(self, data, id=None, name=None):
+        self.data = data
+        self.id = str(uuid.uuid4()) if id is None else id
+        self.name = "test"
+
+    def getAs(self, *args):
+        if type(self.data) == dict:
+            returnData = {}
+            for key in args:
+                if key in self.data:
+                    returnData = returnData | self.data[key]
+            return Data(returnData, self.id)
+
+
+class Prediction:
+    def __init__(self, prediction, id=None):
+        self.data = prediction
+        self.id = str(uuid.uuid4()) if id is None else id
+        self.name = "test"
+
+    def getAs(self, *args):
+        if type(self.data) == dict:
+            returnData = {}
+            for key in args:
+                if key in self.data:
+                    returnData = returnData | self.data[key]
+            return Prediction(returnData, self.id)
+
+
+class DataQualityReport:
+    def __init__(self, report, id=None):
+        self.data = report
+        self.id = str(uuid.uuid4()) if id is None else id
+
+    def getAs(self, *args):
+        if type(self.data) == dict:
+            returnData = {}
+            for key in args:
+                if key in self.data:
+                    returnData = returnData | self.data[key]
+            return DataQualityReport(returnData, self.id)
+
+
+class PredictionQuality:
+    def __init__(self, metrics, id=None):
+        self.data = metrics
+        self.id = str(uuid.uuid4()) if id is None else id
+
+    def getAs(self, *args):
+        if type(self.data) == dict:
+            returnData = {}
+            for key in args:
+                if key in self.data:
+                    returnData = returnData | self.data[key]
+            return DataQualityReport(returnData, self.id)
+
+
+class LineageManager:
+    def __init__(self) -> None:
+        config = "user=nguu0123 password=nguu0123456 host=172.17.0.3 port=5432 dbname=nguu0123"
+        self.connection = psycopg.connect(config, autocommit=True)
+        self.cursor = self.connection.cursor()
+
+    def capturePredictActivity(
+        self,
+        activityId,
+        startTime,
+        endTime,
+        data: Data,
+        prediction: Prediction,
+        predictionCpu,
+        predictionMem,
+        modelId
+    ):
+        postgres_insert_query = (
+            """ INSERT INTO predict (id, start_time, end_time) VALUES (%s,%s,%s)"""
+        )
+        record_to_insert = (activityId, startTime, endTime)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = """ INSERT INTO prediction (id, name) VALUES (%s,%s)"""
+        record_to_insert = (prediction.id, prediction.name)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO wasgeneratedby(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, prediction.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO used(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, data.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        predictionQuality = {"cpu": predictionCpu, "mem": predictionMem}
+        if "QoA" in prediction.data:
+            predictionQuality["QoA"] = prediction.data["QoA"]
+        predictionQualityId = str(uuid.uuid4())
+        postgres_insert_query = (
+            """ INSERT INTO predictionquality(id, value) VALUES (%s,%s)"""
+        )
+        record_to_insert = (predictionQualityId, json.dumps(predictionQuality))
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO wasgeneratedby(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, predictionQualityId)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO wasassociatedwith(activityid, agentid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, modelId)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+    def captureAssessDataQualityActivity(
+        self, activityId, data: Data, dataQualityReport: DataQualityReport
+    ):
+        postgres_insert_query = (
+            """ INSERT INTO dataqualityreport(id, value) VALUES (%s,%s)"""
+        )
+        record_to_insert = (dataQualityReport.id, json.dumps(dataQualityReport.data))
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO assessdataquality(id, name) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, "assess data quality")
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO wasgeneratedby(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, dataQualityReport.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO used(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, data.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+    def capturePreprocessingActivity(
+        self, activityId, inputData: Data, outputData: Data, funcName
+    ):
+        postgres_insert_query = """ INSERT INTO preprocess(id, name) VALUES (%s,%s)"""
+        record_to_insert = (activityId, funcName)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = """ INSERT INTO data(id, name) VALUES (%s,%s)"""
+        record_to_insert = (outputData.id, funcName + "output")
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO wasgeneratedby(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, outputData.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO used(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, inputData.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+    def captureEnsembleActivity(
+        self, activityId, inputs: list[Prediction], output: Prediction, funcName
+    ):
+        postgres_insert_query = (
+            """ INSERT INTO ensemblefunction(id, name) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, funcName)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = """ INSERT INTO prediction(id, name) VALUES (%s,%s)"""
+        record_to_insert = (output.id, funcName + "result")
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        postgres_insert_query = (
+            """ INSERT INTO wasgeneratedby(activityid, entityid) VALUES (%s,%s)"""
+        )
+        record_to_insert = (activityId, output.id)
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+
+        for input in inputs:
+            postgres_insert_query = (
+                """ INSERT INTO used(activityid, entityid) VALUES (%s,%s)"""
+            )
+            record_to_insert = (activityId, input.id)
+            self.cursor.execute(postgres_insert_query, record_to_insert)
+    def getModelId(self, modelName, param):
+        postgreSQL_select_Query = "select * from model where name = %s and parameter ->> 'param' = %s"
+        self.cursor.execute(postgreSQL_select_Query, (modelName, param))
+        mobile_records = self.cursor.fetchall()
+        if self.cursor.rowcount == 0:
+             modelId = str(uuid.uuid4())
+             postgres_insert_query = """ INSERT INTO model(id, name, parameter) VALUES (%s,%s,%s)"""
+             record_to_insert = (modelId, modelName, json.dumps({"param": param}))
+             self.cursor.execute(postgres_insert_query, record_to_insert)
+             return modelId
+        return mobile_records[0][0]
+
+def report_proc_cpu(process):
+    report = {}
+    cpu_time = process.cpu_times()
+    contex = process.num_ctx_switches()
+    for key in cpu_time._fields:
+        report[key] = getattr(cpu_time, key)
+    for key in contex._fields:
+        report[key] = getattr(contex, key)
+    report["num_thread"] = process.num_threads()
+
+    return report
+
+
+def get_proc_cpu(pid=None):
+    if pid == None:
+        pid = os.getpid()
+    process = psutil.Process(pid)
+    return report_proc_cpu(process)
+
+
+def report_proc_mem(process):
+    report = {}
+    mem_info = process.memory_info()
+    for key in mem_info._fields:
+        report[key] = getattr(mem_info, key)
+    return report
+
+
+def get_proc_mem(pid=None):
+    if pid == None:
+        pid = os.getpid()
+    process = psutil.Process(pid)
+    return report_proc_mem(process)
+
+
+
+def capture(activityType): 
+    def wrapper(func):
+        @functools.wraps(func)
+        def doFunc(*args, **kwargs):
+            startTime = datetime.now()
+            beforeCpu = None
+            beforeMem = None
+            if activityType == "predict":
+                beforeCpu = get_proc_cpu()
+                beforeMem = get_proc_mem()
+            returnVal = func(*args, **kwargs)
+            funcName = func.__name__
+            endTime = datetime.now()
+            activityId = str(uuid.uuid4())
+            lineageManager = LineageManager()
+            if activityType == "predict":
+                predictionCpu = {"before": beforeCpu, "after": get_proc_cpu()}
+                predictionMem = {"before": beforeMem, "after": get_proc_mem()}
+                data = kwargs["data"]
+                returnVal = Prediction(returnVal)
+                lineageManager.capturePredictActivity(
+                    activityId,
+                    startTime,
+                    endTime,
+                    data,
+                    returnVal,
+                    predictionCpu,
+                    predictionMem,
+                    args[0].id
+                )
+            elif activityType == "assessDataQuality":
+                data = kwargs["data"]
+                returnVal = DataQualityReport(returnVal)
+                lineageManager.captureAssessDataQualityActivity(
+                    activityId, data, returnVal
+                )
+            elif activityType == "preprocessing":
+                inputData = kwargs["data"]
+                returnVal = Data(returnVal)
+                lineageManager.capturePreprocessingActivity(
+                    activityId, inputData, returnVal, funcName
+                )
+            elif activityType == "ensemble":
+                inputPredictions = kwargs["predictions"]
+                returnVal = Prediction(returnVal)
+                lineageManager.captureEnsembleActivity(
+                    activityId, inputPredictions, returnVal, funcName
+                )
+            return returnVal
+
+        return doFunc
+
+    return wrapper
+
+
+def captureModel(func):
+    @functools.wraps(func)
+    def wrapper_init_model(*args, **kwargs):
+        model = args[0]
+        lineageManager = LineageManager()
+        param = kwargs["param"]
+        model.id = lineageManager.getModelId(model.__class__.__name__, param)
+        return func(*args, **kwargs)
+
+    return wrapper_init_model
+
+
+def captureInputData(data): 
+    returnData = Data(data)
+    lineageManager = LineageManager()
+    postgres_insert_query = """ INSERT INTO data(id, name) VALUES (%s,%s)"""
+    record_to_insert = (returnData.id, "input data")
+    lineageManager.cursor.execute(postgres_insert_query, record_to_insert)
+    return returnData
